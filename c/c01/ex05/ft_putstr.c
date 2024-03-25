3e42dd417d65ba18bc1d5f40969e2648b2278406
emsmccoy <65463392+emsmccoy@users.noreply.github.com>
2024-03-25 19:08:58 +0100

    Update ft_putstr.c

--- c/c01/ex05/ft_putstr.c
+++ c/c01/ex05/ft_putstr.c
@@ -23,3 +23,14 @@ void	ft_putstr(char *str)
 		str++;
 	}
 }
+
+//version 2
+void ft_putstr(char *str)
+{
+    int i = 0;
+    while (str[i])
+    {
+        write(1, &str[i], 1);
+        i++;
+    }
+}
